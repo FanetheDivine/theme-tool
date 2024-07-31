5be4691abe8edf1308771d97574e3631d3693851
Yuanzhuoxin <1974269607@qq.com>
2024-07-31 10:58:36 +0800

    feat: 增加突变时受控的非受控InputNumber组件

---
+++ src/utils/NumberValue/index.tsx
@@ -0,0 +1,14 @@
+import { InputNumber } from "antd"
+import { FC } from "react"
+import { useCompWithMutation } from "../useCompWithMutation"
+
+type NumberValueProps = {
+  value: number,
+  onChange: (newVal: number) => void
+}
+
+/** 仅在value突变时受控的组件 */
+export const NumberValue: FC<NumberValueProps> = props => {
+  const [key, onChange] = useCompWithMutation(props.value, props.onChange)
+  return <InputNumber key={key} defaultValue={props.value} onChange={num => onChange(num ?? 0)}></InputNumber>
+} 
