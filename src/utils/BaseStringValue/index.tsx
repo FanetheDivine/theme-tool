9514ca23e98fb11151d49a470ca33d50a4438d98
Yuanzhuoxin <1974269607@qq.com>
2024-07-31 10:43:23 +0800

    feat: 增加和应用用于控制非受控组件的钩子

--- src/utils/BaseStringValue/index.tsx
+++ src/utils/BaseStringValue/index.tsx
@@ -1,14 +1,15 @@
-import { CSSProperties, FC, memo, useRef, useState } from "react"
+import { CSSProperties, FC, useRef, useState } from "react"
 import tinycolor from "tinycolor2"
 import { ColorPicker, Input } from 'antd'
-import { usePrevious } from "ahooks"
+import { useCompWithMutation } from "../useCompWithMutation"
+import { useCreation } from "ahooks"
 
 type BaseStringValueProps = {
-  /** 显示在input中的文字 */
+  /** 显示在input中的文字 默认为value */
   text?: string,
-  /** 实际的值 可以被解析为颜色 */
+  /** 实际值 可以被解析为颜色 */
   value: string,
-  onChange?: (newValue: string) => void,
+  onChange: (newValue: string) => void,
   style?: CSSProperties,
   className?: string
 }
@@ -21,48 +22,30 @@ type BaseStringValueProps = {
  * @example <BaseStringValue value={value} onChange={debounce(setValue, 1000)} />
 */
 export const BaseStringValue: FC<BaseStringValueProps> = props => {
-  const changedValueRef = useRef<string | tinycolor.Instance>()
-  const prevValue = usePrevious(props.value)
-  const keyRef = useRef<string>()
-  const curValue = tinycolor(props.value)
-  if (prevValue !== props.value) {
-    const equal = (typeof changedValueRef.current === 'string' && !curValue.isValid() && changedValueRef.current === props.value)
-      || (typeof changedValueRef.current !== 'string' && curValue.isValid() && tinycolor.equals(changedValueRef.current, curValue))
-    if (!equal) {
-      keyRef.current = Date.now().toString()
-    }
-  }
-
+  const [key, onInnerChange] = useCompWithMutation(props.value, props.onChange, isEqual)
   return (
-    <BaseStringValueInner key={keyRef.current}
-      style={props.style} className={props.className}
-      defaultText={props.text ?? props.value} defaultValue={props.value}
-      onChange={v => {
-        const color = tinycolor(v)
-        changedValueRef.current = color.isValid() ? color : v
-        props.onChange?.(v)
-      }}
+    <BaseStringValueInner key={key}
+      defaultText={props.text} defaultValue={props.value}
+      onChange={onInnerChange}
     />
   )
 }
 
-
 type BaseStringValueInnerProps = {
   /** 初始显示在input中的文字 */
-  defaultText: string,
+  defaultText?: string,
   /** 初始实际的值 可以被解析为颜色 */
   defaultValue: string,
-  onChange?: (newValue: string) => void,
+  onChange: (newValue: string) => void,
   style?: CSSProperties,
   className?: string
 }
 
 const BaseStringValueInner: FC<BaseStringValueInnerProps> = props => {
-  const [text, setText] = useState(props.defaultText)
-  const defaultColorRef = useRef(props.defaultValue)
-  const isColor = tinycolor(defaultColorRef.current).isValid()
+  const [text, setText] = useState(props.defaultText ?? props.defaultValue)
+  const [color, setColor] = useState(props.defaultValue)
   const colorSpan = (
-    <ColorPicker defaultValue={props.defaultValue} format='rgb'
+    <ColorPicker value={color} format='rgb'
       onChange={v => {
         const colorString = v.toRgbString()
         props.onChange?.(colorString)
@@ -71,6 +54,10 @@ const BaseStringValueInner: FC<BaseStringValueInnerProps> = props => {
     />
   )
 
+  const isColor = useCreation(() => {
+    return tinycolor(props.defaultValue).isValid()
+  }, [])
+
   return (
     <Input style={props.style} className={props.className}
       value={text} suffix={isColor ? colorSpan : null}
@@ -78,7 +65,20 @@ const BaseStringValueInner: FC<BaseStringValueInnerProps> = props => {
         const newText = e.target.value
         props.onChange?.(newText)
         setText(newText)
+        if (tinycolor(newText).isValid()) {
+          setColor(newText)
+        }
       }}
     />
   )
+}
+
+/** 判断可能为颜色的字符串是否相等 */
+const isEqual = (val1?: string, val2?: string) => {
+  if (val1 === val2) return true
+  if (val1 === undefined || val2 === undefined) return false
+  const color1 = tinycolor(val1)
+  const color2 = tinycolor(val2)
+  if (!color1.isValid() || !color2.isValid()) return false
+  return tinycolor.equals(color1, color2)
 }
---
+++ src/utils/useCompWithMutation/index.tsx
@@ -0,0 +1,36 @@
+import { useCallback, useRef } from "react"
+
+/**
+ * 使非受控组件的在value突变时重建,达到类似受控的效果
+ * @param value 监听突变的值
+ * @param onChange 变更函数
+ * @param isEqual 比较函数.默认Object.is
+ * @example 
+ * const InputWithMutation:FC<{value:string,onChange:(newVal:string)=>void}> = props=>{
+ *   const [key,onInnerChange] = useCompWithMutation(props.value,props.onChange)
+ *   return <input key={key} defaultValue={props.value} onChange={e => onInnerChange(e.target.value)} />
+ * }
+ * 
+ * const Comp:FC=()=>{
+ *  const [text,setText] = useState('')
+ *  return (
+ *    <>
+ *      <button onClick={()=>setText('reset')}>reset</button>
+ *      <InputWithMutation value={text} onChange={setText}/>
+ *    </>
+ *  )
+ * }
+ */
+export function useCompWithMutation<T, V = T>(value: T, onChange: (newVal: V) => void, isEqual?: (val1?: T, val2?: V) => boolean) {
+  const keyRef = useRef<string>()
+  const changedValueRef = useRef<V>()
+  const onInnerChange = useCallback((newVal: V) => {
+    changedValueRef.current = newVal
+    onChange(newVal)
+  }, [onChange])
+  const equalFn = isEqual ?? Object.is.bind(Object)
+  if (!equalFn(value, changedValueRef.current)) {
+    keyRef.current = Date.now().toString()
+  }
+  return [keyRef.current, onInnerChange] as const
+}
