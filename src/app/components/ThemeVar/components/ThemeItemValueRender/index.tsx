547b5f353db22d8a5240517725ade13f7ba2c6ba
Yuanzhuoxin <1974269607@qq.com>
2024-08-01 14:14:56 +0800

    perf: 优化主题元展示的样式

--- src/app/components/ThemeVar/components/ThemeItemValueRender/index.tsx
+++ src/app/components/ThemeVar/components/ThemeItemValueRender/index.tsx
@@ -33,8 +33,8 @@ export function ThemeItemValueRender<T>(props: ThemeItemValueRenderProps<T>) {
     )
     return (
       <Popover trigger={'click'} content={content} placement='right'>
-        <div className='flex justify-around items-center cursor-pointer text-sm'>
-          <span>数组[...]</span>
+        <div className='flex items-center cursor-pointer text-sm'>
+          <span className='flex-auto text-center'>数组[...]</span>
           <RightOutlined></RightOutlined>
         </div>
       </Popover>
