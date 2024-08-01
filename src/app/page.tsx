b0fa39d15e04b65f3a6327264dbbae1423f87f4d
Yuanzhuoxin <1974269607@qq.com>
2024-08-01 15:11:25 +0800

    perf: 优化组件的展示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { ThemeMap } from './components/ThemeMap'
 const Page: FC = () => {
   return (
     <main className='flex w-full h-full'>
-      <ThemeVar className='flex-grow-0 w-40 px-2 py-1'></ThemeVar>
+      <ThemeVar className='flex-grow-0 w-48 px-2 py-1'></ThemeVar>
       <Divider type='vertical' className='h-full bg-gray'></Divider>
       <ThemeContent className='flex-auto'></ThemeContent>
       <Divider type='vertical' className='h-full bg-gray'></Divider>
