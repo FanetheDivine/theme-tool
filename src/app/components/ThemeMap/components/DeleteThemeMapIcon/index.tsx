591aed42e43ef982a51a3750c25b9ba823f3be84
Yuanzhuoxin <1974269607@qq.com>
2024-08-02 13:38:57 +0800

    chore: 调整提示语句

--- src/app/components/ThemeMap/components/DeleteThemeMapIcon/index.tsx
+++ src/app/components/ThemeMap/components/DeleteThemeMapIcon/index.tsx
@@ -19,7 +19,7 @@ export const DeleteThemeMapIcon: FC<DeleteThemeMapIconProps> = props => {
         if (!isOriginThemeMap(props.themeMapKey, themeInfo.themeMap, themeInfo.themeMapEditRecorder)) {
           Modal.confirm({
             title: '确认删除?',
-            content: '这个映射不在初始的主题映射中,它的删除是不可逆的.',
+            content: '这个操作会删除此映射及它的所有下级映射.这个映射不在初始的主题映射中,它的删除是不可逆的.',
             onOk: deleteThemeMap
           })
         } else {
