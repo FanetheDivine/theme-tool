1a9869d3521ff411d358d1d71426c9369f35e24f
Yuanzhuoxin <1974269607@qq.com>
2024-08-02 14:59:11 +0800

    chore: 调整初始映射使其更易于展示

--- src/utils/theme/index.tsx
+++ src/utils/theme/index.tsx
@@ -4,16 +4,16 @@ import { createTheme } from "@/lib/Theme"
 
 export const { ThemeProvider, useTheme } = createTheme({
   themeVar: new Map([
-    ['@a', { desc: '主题元@a', value: [1, 'a', ['t']] }],
+    ['@a', { desc: '主题元@a', value: [1, 'a'] }],
     ['@b', { desc: '主题元@b', value: '#FFFFFF' }]
   ]),
   themeMap: new Map([
-    ['example', { desc: '示例', value: ['t', ['@a', '@b-G9-A90']] }],
+    ['example', { desc: '示例', value: ['@a', '@b-G9-A90'] }],
     [
       'example1',
       {
         desc: '具有子结构的映射',
-        children: new Map([['sub', { desc: 'sub', value: 1 }]])
+        children: new Map([['sub', { desc: '子映射', value: 1 }]])
       }
     ]
   ])
